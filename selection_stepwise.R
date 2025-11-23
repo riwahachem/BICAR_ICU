66ed2fe0665a3f5fdec8761c8a0df313cdc36a5c
Riwa Hachem Reda <riwahachemreda@gmail.com>
2025-11-23 13:59:49 +0100

    Create selection_stepwise.R

---
+++ selection_stepwise.R
@@ -0,0 +1,28 @@
+data = read.csv("data.csv", header = TRUE, sep = " ")
+
+# Modèle avec les variables obligatoires
+null_model <- glm(
+  PNEUMONIA_YN ~ ARM_NUM + INCL_SEPSIS_YN + INCL_AKIN + AGE + SEX + BMI + SOFA,
+  data = data,
+  family = binomial
+)
+
+# Modèle complet
+full_model <- glm(
+  PNEUMONIA_YN ~ ., 
+  data = data, 
+  family = binomial
+)
+
+# Stepwise 
+stepwise_model <- step(
+  null_model,
+  scope = list(
+    lower = formula(null_model),
+    upper = formula(full_model)
+  ),
+  direction = "both"
+)
+
+vars_stepwise <- attr(terms(stepwise_model), "term.labels")
+vars_stepwise
