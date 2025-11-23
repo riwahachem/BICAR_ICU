4c71cf903bd050c137c8065237536150d476fd9b
mgermain12 <marine.germain@etu.umontpellier.fr>
2025-11-23 11:59:27 +0100

    backward selection

--- selection_backward.R
+++ selection_backward.R
@@ -15,7 +15,7 @@ candidats <- setdiff(names(data), c("PNEUMONIA_YN", vars_obligatoires))
 #cette ligne transforme c("X1","X2") en P_YN ~ X1+X2 (pour pouvoir appliquer glm ensuite)
 formule_init <- as.formula(paste("PNEUMONIA_YN ~", paste(candidats, collapse = " + ")))
 
-# Point de départ avec toutes les variables candidates
+# Modèle de départ avec toutes les variables candidates
 mod_init <- glm(formule_init, data = data, family = binomial)
 
 # Selection backward avec critère AIC, suppression d'une variable à la fois pour voir si l'AIC diminue
@@ -30,11 +30,9 @@ backward_aic <- stepAIC(mod_init, direction = "backward", trace = TRUE)
 #Variables selectionnées parmi les variables candidates
 candidats_selec <- attr(terms(backward_aic), "term.labels")
 
-# Modèle final en ajoutant les variables forcées
+# Modèle final à utiliser pour la régression logistique
 vars_finales <- c(candidats_selec, vars_obligatoires)
 formule_finale <- as.formula(paste("PNEUMONIA_YN ~", paste(vars_finales, collapse = " + ")))
-
-# Modèle final (fit ultérieurement pour interprétation/prédiction)
 final_model <- glm(formule_finale, data = data, family = binomial)
 
 # Variables retenues
