6e2c51fdb202543eb7875b394b4f869a087fe33c
mgermain12 <marine.germain@etu.umontpellier.fr>
2025-11-26 09:45:59 +0100

    correction selection backward

--- Selection_variables/selection_backward.R
+++ Selection_variables/selection_backward.R
@@ -16,8 +16,9 @@ backward_bic <- stepAIC(mod_full, direction = "backward", k = log(n), trace = TR
 candidats_selec_aic <- attr(terms(backward_aic), "term.labels")
 candidats_selec_bic <- attr(terms(backward_bic), "term.labels")
 
-vars_backward_aic <- c(candidats_selec_aic, vars_obligatoires)
-vars_backward_bic <- c(candidats_selec_bic, vars_obligatoires)
+vars_backward_aic <- unique(c(candidats_selec_aic, vars_obligatoires))
+
+vars_backward_bic <- unique(c(candidats_selec_bic, vars_obligatoires))
 
 save(vars_backward_aic, file = "Selection_variables/vars_backward_aic.RData")
 save(vars_backward_bic, file = "Selection_variables/vars_backward_bic.RData")
