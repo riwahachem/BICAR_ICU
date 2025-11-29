6e200feed8ae7832264ede9950b288c8fd127406
mgermain12 <marine.germain@etu.umontpellier.fr>
2025-11-29 10:50:15 +0100

    commentaire des codes du dossier Traitement

--- Traitement/analyse_descriptive.R
+++ Traitement/analyse_descriptive.R
@@ -1,13 +1,28 @@
 library(ggplot2)
 library(dplyr)
 
+################################################################################
+
+#Pré-requis : Exécution du fichier Traitement/nettoyage.R pour générer data.csv 
+
+# Ce script contient  : 
+# 1) Le chargement de la table data
+# 2) La description de la variable cible PNEUMONIA_YN
+# 3) La visualisation du lien entre les variables qualitative et la variable cible (barblot)
+# 4) La conversion des variables si besoin
+# 5) La visualisation du lien entre les variables quantitatives et la variable cible (boxblot)
+
+################################################################################
+
+
 data = read.csv2("data.csv",sep=",")
+
 Y = data$PNEUMONIA_YN
 Y = as.factor(Y)
-
-# Analyse variable cible
 table(Y)
 prop.table(table(Y))
+
+#Visualisation de la variable SEX vs la variable cible
 
 ggplot(data, aes(x = factor(SEX), fill = factor(PNEUMONIA_YN))) +
   geom_bar(position = "fill") +
@@ -19,15 +34,19 @@ ggplot(data, aes(x = factor(SEX), fill = factor(PNEUMONIA_YN))) +
     fill = "Pneumonie"
   )
 
+#Visualisation de la variable ADMIN_REASON vs la variable cible
+
 ggplot(data, aes(x = factor(ADMIN_REASON), fill = factor(PNEUMONIA_YN))) +
   geom_bar(position = "fill") +
   scale_fill_manual(values = c("0" = "steelblue", "1" = "darkred")) +
   labs(
-    title = "Nombre de patients avec et sans pneumonie selon la raison de la venue",
+    title = "Nombre de patients avec et sans pneumonie selon la raison de l'admission",
     x = "Raison médicale ou chirurgicale",
     y = "Proportion",
     fill = "Pneumonie"
   )
+
+#Visualisation de la variable ATCD_Smoking vs la variable cible
 
 ggplot(data, aes(x = factor(ATCD_Smoking), fill = factor(PNEUMONIA_YN))) +
   geom_bar(position = "fill") +
@@ -41,6 +60,8 @@ ggplot(data, aes(x = factor(ATCD_Smoking), fill = factor(PNEUMONIA_YN))) +
 
 data$INCL_IMV <- as.factor(data$INCL_IMV)
 
+#Visualisation de la variable INCL_IMV vs la variable cible
+
 ggplot(data, aes(x = factor(INCL_IMV), fill = factor(PNEUMONIA_YN))) +
   geom_bar(position = "fill") +
   scale_fill_manual(values = c("0" = "steelblue", "1" = "darkred")) +
@@ -51,6 +72,8 @@ ggplot(data, aes(x = factor(INCL_IMV), fill = factor(PNEUMONIA_YN))) +
     fill = "Pneumonie"
   )
 
+#Visualisation de la variable ARM_NUM vs la variable cible
+
 ggplot(data, aes(x = factor(ARM_NUM), fill = factor(PNEUMONIA_YN))) +
   geom_bar(position = "fill") +
   scale_fill_manual(values = c("0" = "steelblue", "1" = "darkred")) +
@@ -59,7 +82,9 @@ ggplot(data, aes(x = factor(ARM_NUM), fill = factor(PNEUMONIA_YN))) +
     x = "Groupe",
     y = "Proportion",
     fill = "Pneumonie"
-  )
+  ) 
+
+#Visualisation de la variable SOFA vs la variable cible
 
 ggplot(data, aes(x = factor(PNEUMONIA_YN), y = SOFA, fill = factor(PNEUMONIA_YN))) +
   geom_boxplot() +
@@ -68,7 +93,10 @@ ggplot(data, aes(x = factor(PNEUMONIA_YN), y = SOFA, fill = factor(PNEUMONIA_YN)
     title = "Distribution du score SOFA selon la pneumonie",
     x = "Pneumonie",
     y = "SOFA"
-  )
+  ) +
+  theme(legend.position = "none")
+
+#Visualisation de la variable AGE vs la variable cible
 
 ggplot(data, aes(x = factor(PNEUMONIA_YN), y = AGE, fill = factor(PNEUMONIA_YN))) +
   geom_boxplot() +
@@ -77,18 +105,20 @@ ggplot(data, aes(x = factor(PNEUMONIA_YN), y = AGE, fill = factor(PNEUMONIA_YN))
     title = "Distribution de l'âge selon la pneumonie",
     x = "Pneumonie",
     y = "AGE"
-  )
+  ) +
+  theme(legend.position = "none")
 
 data$BMI <- as.numeric(data$BMI)
 
-ggplot(data, aes(x = factor(PNEUMONIA_YN), y = BMI, fill = factor(PNEUMONIA_YN))) +
+#Visualisation de la variable BMI vs la variable cible
+
+ggplot(data, aes(x = factor(PNEUMONIA_YN), y = BMI, fill = factor(PNEUMONIA_YN)
+                 ))+
   geom_boxplot() +
   labs(
     title = "Distribution du BMI selon la pneumonie",
     x = "Pneumonie",
     y = "BMI"
   ) +
-  scale_fill_manual(values = c("0" = "steelblue", "1" = "darkred"))
-
-
-
+  scale_fill_manual(values = c("0" = "steelblue", "1" = "darkred")) +
+  theme(legend.position = "none")
--- Traitement/nettoyage.R
+++ Traitement/nettoyage.R
@@ -1,33 +1,56 @@
 library(dplyr)
+library(lubridate)
 
-admission = read.csv2("Data/ADMISSION.csv", sep =',')
-admission = admission[,c("SUBJID", "ADMIN_SURGICAL", "Acidaemia_reason_admin")]
+################################################################################
+
+#Pré-requis : télécharger toutes les bases de données du CRF 
+
+# Thème : prédiction des infections pulmonaires
+
+# Ce script contient  : 
+# 1) Le chargement des tables
+# 2) L'extraction des variables utiles pour chaque table
+# 3) La préparation des données 
+# 4) La jointure des tables 
+# 5) La création de la table finale
+
+################################################################################
+
+# Renommage de la variable ADMIN_SURGICAL en ADMIN_REASON :
+# (0 = admission médicale, 1 = admission chirurgicale)
+
+admission = read.csv2("ADMISSION.csv", sep =',')
+admission = admission[,c(2,6,7)]
 admission = admission %>% rename(ADMIN_REASON=ADMIN_SURGICAL)
 
-baseline_clin = read.csv2("Data/BASELINE_CLIN.csv", sep =',')
+baseline_clin = read.csv2("BASELINE_CLIN.csv", sep =',')
 baseline_clin = baseline_clin[,c(2, 5:18)]
 
-biology = read.csv2("Data/BIOLOGY.csv", sep =',')
+biology = read.csv2("BIOLOGY.csv", sep =',')
 biology = biology[, c(2, 5:11)]
 
-inclusion = read.csv2("Data/INCLUSION.csv", sep =',')
+inclusion = read.csv2("INCLUSION.csv", sep =',')
 inclusion = inclusion[, c(2,5:7)]
 
-nosocomial = read.csv2("Data/NOSOCOMIAL.csv", sep =',')
+nosocomial = read.csv2("NOSOCOMIAL.csv", sep =',')
 nosocomial = nosocomial[, c(2,5)]
 
-randomization = read.csv2("Data/RANDOMIZATION.csv", sep =',')
+randomization = read.csv2("RANDOMIZATION.csv", sep =',')
 randomization = randomization[, c(2, 5:7, 10)]
 
-sofa = read.csv2("Data/SOFA.csv", sep =',')
+# Calcul du score SOFA à l'admission (somme des composants)
+
+sofa = read.csv2("SOFA.csv", sep =',')
 sofa = sofa[, c(2, 5:10)]
 SOFA = rowSums(sofa[,2:7])
 sofa = data.frame(SUBJID = sofa[,"SUBJID"], SOFA)
 
+# Jointure des tables sur la variable d'identification SUBJID
+
 table = list(admission, baseline_clin, biology, inclusion, nosocomial, randomization, sofa)
 data = Reduce(function(x,y) full_join(x,y, by = "SUBJID"), table)
 
-library(lubridate)
+# Création de la variable AGE (différence entre la date d'admission et la date de naissance)
 
 data <- data %>%
   mutate(
@@ -35,7 +58,11 @@ data <- data %>%
   ) %>%
   dplyr::select(-BIRTH, -CONSENT_DATE)
 
+# Conversion des variables character en numérique pour la cohérence des formats
+
 data <- data %>%
   mutate(across(.cols = where(is.character), .fns = as.numeric))
+
+# Création de la table finale
 
 write.csv(data, "data.csv", row.names = FALSE)
